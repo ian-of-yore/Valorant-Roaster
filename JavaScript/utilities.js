fb987589a947a36f962bf9b7f5768968f47ee86d
Ijaj Ahamed Nabil <37688324+ian-of-yore@users.noreply.github.com>
2022-08-22 00:56:07 +0600

    made some common functions to make life easy xD

---
+++ JavaScript/utilities.js
@@ -0,0 +1,31 @@
+// convert the innerText to an integer value
+function setInnertextToInteger(input) {
+    const objectValue = document.getElementById(input);
+    const objectValueString = objectValue.innerText;
+    return objectValueInt = parseInt(objectValueString);
+}
+
+// set a value as innerText of an element
+function setInnertextAsValue(elementId, elementValue) {
+    const element = document.getElementById(elementId);
+    return element.innerText = elementValue;
+}
+
+// Collect input value and convert it to integer
+function collectInputValueAsInteger(objectId) {
+    const objectValue = document.getElementById(objectId);
+    const objectValueString = objectValue.value;
+    return objectValueInt = parseInt(objectValueString);
+}
+
+// set inputValue to an element
+function setInputValueToElement(objectId, objectValue) {
+    const element = document.getElementById(objectId);
+    return element.value = objectValue;
+}
+
+// collect innerText from element
+function collectInnerText(objectId) {
+    const objectValue = document.getElementById(objectId);
+    return objectValue.innerText;
+}
