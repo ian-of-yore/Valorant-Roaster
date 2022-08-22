fe37706c1cd2f8be5d2e4ae5307761ca720e8b26
Ijaj Ahamed Nabil <37688324+ian-of-yore@users.noreply.github.com>
2022-08-22 17:44:32 +0600

    whole assignment done, might do some polishing later

--- JavaScript/budget-calculation.js
+++ JavaScript/budget-calculation.js
@@ -1,7 +1,22 @@
-// total budget for all selected players
-document.getElementById("calculate-players-budget").addEventListener("click", function () {
+// function to calculate the budget for all selected players
+function totalPlayerExpenses() {
     const perPlayerCost = collectInputValueAsInteger("cost-per-player");
     const totalPlayers = listContainerLength();
-    const playerExpenses = perPlayerCost * totalPlayers;
-    setInnertextAsValue("player-expenses", playerExpenses);
+    return playerExpenses = perPlayerCost * totalPlayers;
+}
+
+
+// budget for all selected players
+document.getElementById("calculate-players-budget").addEventListener("click", function () {
+    const playerExpenses = totalPlayerExpenses();
+    setInnertextAsValue("player-expenses", playerExpenses)
+})
+
+// Total budget for the roaster including players, managers and coach
+document.getElementById("calculate-total-budget").addEventListener("click", function () {
+    const managerCost = collectInputValueAsInteger("cost-manager");
+    const coachCost = collectInputValueAsInteger("cost-coach");
+    const playersExpenses = totalPlayerExpenses();
+    const calculateTotalBudget = playersExpenses + managerCost + coachCost;
+    setInnertextAsValue("total-expenses", calculateTotalBudget)
 })
