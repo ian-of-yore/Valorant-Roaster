4931f681b2aea5751adb821c81e85aa85dd8f15d
Ijaj Ahamed Nabil <37688324+ian-of-yore@users.noreply.github.com>
2022-08-22 16:43:52 +0600

    all players connected to the list container

--- JavaScript/update-cart.js
+++ JavaScript/update-cart.js
@@ -1,4 +1,53 @@
+
+// Player: Scream
 document.getElementById("btn-scream").addEventListener("click", function () {
     addPlayerToListContainer("name-scream", "list-container");
     disableSelectButton("btn-scream");
+})
+
+// Player: Jamppi
+document.getElementById("btn-jamppi").addEventListener("click", function () {
+    addPlayerToListContainer("name-jamppi", "list-container");
+    disableSelectButton("btn-jamppi");
+})
+
+// Player: Boaster
+document.getElementById("btn-boaster").addEventListener("click", function () {
+    addPlayerToListContainer("name-boaster", "list-container");
+    disableSelectButton("btn-boaster");
+})
+
+// Player: nAts
+document.getElementById("btn-nats").addEventListener("click", function () {
+    addPlayerToListContainer("name-nats", "list-container");
+    disableSelectButton("btn-nats");
+})
+
+// Player: nabil
+document.getElementById("btn-nabil").addEventListener("click", function () {
+    addPlayerToListContainer("name-nabil", "list-container");
+    disableSelectButton("btn-nabil");
+})
+
+// Player: asuna
+document.getElementById("btn-asuna").addEventListener("click", function () {
+    addPlayerToListContainer("name-asuna", "list-container");
+    disableSelectButton("btn-asuna");
+})
+
+// Player: averageJonas
+document.getElementById("btn-jonas").addEventListener("click", function () {
+    addPlayerToListContainer("name-jonas", "list-container");
+    disableSelectButton("btn-jonas");
+})
+
+// Player: sinatraa
+document.getElementById("btn-sinatraa").addEventListener("click", function () {
+    addPlayerToListContainer("name-sinatraa", "list-container");
+    disableSelectButton("btn-sinatraa");
+})
+// Player: subroza
+document.getElementById("btn-subroza").addEventListener("click", function () {
+    addPlayerToListContainer("name-subroza", "list-container");
+    disableSelectButton("btn-subroza");
 })
